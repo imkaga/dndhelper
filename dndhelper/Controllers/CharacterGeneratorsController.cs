7855c70576be3eed17237c522ba286791caef0e8
imkaga <kacpertom28@gmail.com>
2023-05-23 18:53:34 +0200

    Małe poprawki

--- dndhelper/Controllers/CharacterGeneratorsController.cs
+++ dndhelper/Controllers/CharacterGeneratorsController.cs
@@ -89,58 +89,62 @@ namespace dndhelper.Controllers
             return View(characterGeneratorModel);
         }
 
+        /*
+         * 
+         * 
+         *  EDYTOWANIE WYŁĄCZONE 
+       // GET: CharacterGenerators/Edit/5
+       public async Task<IActionResult> Edit(int? id)
+       {
+           if (id == null || _context.CharacterGenerator == null)
+           {
+               return NotFound();
+           }
 
+           var characterGenerator = await _context.CharacterGenerator.FindAsync(id);
+           if (characterGenerator == null)
+           {
+               return NotFound();
+           }
+           return View(characterGenerator);
+       }
 
-        // GET: CharacterGenerators/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.CharacterGenerator == null)
-            {
-                return NotFound();
-            }
+       // POST: CharacterGenerators/Edit/5
+       // To protect from overposting attacks, enable the specific properties you want to bind to.
+       // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+       [HttpPost]
+       [ValidateAntiForgeryToken]
+       public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Race,Strength,Dexterity,Intelligence,Luck")] CharacterGenerator characterGenerator)
+       {
+           if (id != characterGenerator.Id)
+           {
+               return NotFound();
+           }
 
-            var characterGenerator = await _context.CharacterGenerator.FindAsync(id);
-            if (characterGenerator == null)
-            {
-                return NotFound();
-            }
-            return View(characterGenerator);
-        }
+           if (ModelState.IsValid)
+           {
+               try
+               {
+                   _context.Add(characterGenerator);
+                   await _context.SaveChangesAsync();
+               }
+               catch (DbUpdateConcurrencyException)
+               {
+                   if (!CharacterGeneratorExists(characterGenerator.Id))
+                   {
+                       return NotFound();
+                   }
+                   else
+                   {
+                       throw;
+                   }
+               }
+               return RedirectToAction(nameof(Index));
+           }
+           return View(characterGenerator);
+       }
+       */
 
-        // POST: CharacterGenerators/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Race,Strength,Dexterity,Intelligence,Luck")] CharacterGenerator characterGenerator)
-        {
-            if (id != characterGenerator.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Add(characterGenerator);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CharacterGeneratorExists(characterGenerator.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(characterGenerator);
-        }
 
         // GET: CharacterGenerators/Delete/5
         public async Task<IActionResult> Delete(int? id)
